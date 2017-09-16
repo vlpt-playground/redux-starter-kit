70d41f5c1170680835d331e5522e1a6572c099e2
velopert <public.velopert@gmail.com>
2017-09-16 17:15:50 +0900

    redux-thunk

--- src/App.js
+++ src/App.js
@@ -2,18 +2,44 @@ import React, { Component } from 'react';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import * as counterActions from './modules/counter';
+import * as postActions from './modules/post';
+
+import axios from 'axios';
 
 
 class App extends Component {
-    render() {
-        const { CounterActions, number } = this.props;
 
+    componentDidMount() {
+        const { number, PostActions } = this.props;
+        PostActions.getPost(number);
+    }
+
+    componentWillReceiveProps(nextProps) {
+        const { PostActions } = this.props;
+
+        // 현재 number 와 새로 받을 number 가 다를 경우에 요청을 시도합니다.
+        if(this.props.number !== nextProps.number) {
+            PostActions.getPost(nextProps.number);
+        }
+    }
+
+    render() {
+        const { CounterActions, number, post, error, loading } = this.props;
         
         return (
             <div>
-                <h1>{number}</h1>
+                <p>{number}</p>
                 <button onClick={CounterActions.increment}>+</button>
                 <button onClick={CounterActions.decrement}>-</button>
+                { loading && <h2>로딩중...</h2>}
+                { error 
+                    ? <h1>에러발생!</h1> 
+                    : (
+                        <div>
+                            <h1>{post.title}</h1>
+                            <p>{post.body}</p>
+                        </div>
+                    )}
             </div>
         );
     }
@@ -21,9 +47,13 @@ class App extends Component {
 
 export default connect(
     (state) => ({
-        number: state.counter
+        number: state.counter,
+        post: state.post.data,
+        loading: state.post.pending,
+        error: state.post.error
     }),
     (dispatch) => ({
-        CounterActions: bindActionCreators(counterActions, dispatch)
+        CounterActions: bindActionCreators(counterActions, dispatch),
+        PostActions: bindActionCreators(postActions, dispatch)
     })
 )(App);
--- src/store.js
+++ src/store.js
@@ -1,6 +1,11 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import modules from './modules';
+import { createLogger } from 'redux-logger';
+import ReduxThunk from 'redux-thunk';
 
-const store = createStore(modules)
+
+const logger = createLogger();
+
+const store = createStore(modules, applyMiddleware(logger, ReduxThunk));
 
 export default store;
