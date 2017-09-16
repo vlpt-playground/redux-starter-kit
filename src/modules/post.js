3ed3a37db2dc7e2a691d2e139a23f8bd68352c4b
velopert <public.velopert@gmail.com>
2017-09-16 17:32:17 +0900

    redux-promise-middleware

--- src/modules/post.js
+++ src/modules/post.js
@@ -6,35 +6,15 @@ function getPostAPI(postId) {
     return axios.get(`https://jsonplaceholder.typicode.com/posts/${postId}`)
 }
 
+const GET_POST = 'GET_POST';
 const GET_POST_PENDING = 'GET_POST_PENDING';
 const GET_POST_SUCCESS = 'GET_POST_SUCCESS';
 const GET_POST_FAILURE = 'GET_POST_FAILURE';
 
-export const getPost = (postId) => dispatch => {
-    // 먼저, 요청이 시작했다는것을 알립니다
-    dispatch({type: GET_POST_PENDING});
-
-    // 요청을 시작합니다
-    // 여기서 만든 promise 를 return 해줘야, 나중에 컴포넌트에서 호출 할 때 getPost().then(...) 을 할 수 있습니다
-    return getPostAPI(postId).then(
-        (response) => {
-            // 요청이 성공했을경우, 서버 응답내용을 payload 로 설정하여 GET_POST_SUCCESS 액션을 디스패치합니다.
-            dispatch({
-                type: GET_POST_SUCCESS,
-                payload: response
-            })
-        }
-    ).catch(error => {
-        // 에러가 발생했을 경우, 에로 내용을 payload 로 설정하여 GET_POST_FAILURE 액션을 디스패치합니다.
-        dispatch({
-            type: GET_POST_FAILURE,
-            payload: error
-        });
-        // error 를 throw 하여, 이 함수가 실행 된 다음에 다시한번 catch 를 할 수 있게 합니다.
-        throw(error);
-    })
-
-}
+export const getPost = (postId) => ({
+  type: GET_POST,
+  payload: getPostAPI(postId)
+});
 
 const initialState = {
     pending: false,
--- src/store.js
+++ src/store.js
@@ -2,10 +2,15 @@ import { createStore, applyMiddleware } from 'redux';
 import modules from './modules';
 import { createLogger } from 'redux-logger';
 import ReduxThunk from 'redux-thunk';
+import promiseMiddleware from 'redux-promise-middleware';
+
+const customizedPromiseMiddleware = promiseMiddleware({
+  promiseTypeSuffixes: ['PENDING', 'SUCCESS', 'FAILURE']
+});
 
 
 const logger = createLogger();
 
-const store = createStore(modules, applyMiddleware(logger, ReduxThunk));
+const store = createStore(modules, applyMiddleware(logger, ReduxThunk, customizedPromiseMiddleware));
 
 export default store;
